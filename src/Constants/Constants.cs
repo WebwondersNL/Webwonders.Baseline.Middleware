be41d0fad655b784cf327f916a3b51d4c38b8691
TomChanceWebwonders <tom@webwonders.nl>
2025-10-06 13:12:22 +0200

    Initial commit

---
+++ src/Constants/Constants.cs
@@ -0,0 +1,30 @@
+﻿namespace Webwonders.Baseline.Middleware;
+
+public static partial class Constants
+{
+    public static class Headers
+    {
+        public const string Hsts = "Enable_HSTS";
+        public const string XFrameOptions = "Enable_X-Frame-Options";
+        public const string XContentTypeOptions = "Enable_X-ContentType-Options";
+        public const string XxssProtection = "Enable_X-XSS-Protection";
+        
+        public static class XFrameOptionsValues
+        {
+            public const string SameOrigin = "SAMEORIGIN";
+            public const string Deny = "DENY";
+            public const string AllowFrom = "ALLOW-FROM";
+        }
+        
+        public static class XContentTypeOptionsValues
+        {
+            public const string Nosniff = "nosniff";
+        }
+        
+        public static class XxssProtectionValues
+        {
+            public const string Enabled = "1; mode=block";
+            public const string Disabled = "0";
+        }
+    }
+}
---
+++ src/Headers/HeaderMiddleware.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Polly;
+
+namespace Webwonders.Baseline.Middleware.Headers;
+
+public static class HeaderMiddleware
+{
+    public static WebApplication ConfigureHeaders(this WebApplication app, IWebHostEnvironment env, IConfiguration configuration)
+    {
+        if (env.IsProduction())
+        {
+            // Get the setting from appsettings whether this should be on or not
+            var configSection = configuration.GetSection("Webwonders:Middleware");
+
+            if (configSection != null)
+            {
+                app.Use(async (context, next) =>
+                {
+                    var xContentTypeOption = configSection[Constants.Headers.XContentTypeOptions];
+                    if (xContentTypeOption == Constants.Headers.XContentTypeOptionsValues.Nosniff)
+                    {
+                        context.Response.Headers.Append("X-Content-Type-Options", xContentTypeOption);
+                    }
+
+                    var xFrameOption = configSection[Constants.Headers.XFrameOptions];
+                    if (xFrameOption == Constants.Headers.XFrameOptionsValues.SameOrigin ||
+                        xFrameOption == Constants.Headers.XFrameOptionsValues.Deny ||
+                        xFrameOption == Constants.Headers.XFrameOptionsValues.AllowFrom)
+                    {
+                        context.Response.Headers.Append("X-Frame-Options", xFrameOption);
+                    }
+
+                    var xssProtection = configSection[Constants.Headers.XxssProtection];
+                    if (xssProtection == Constants.Headers.XxssProtectionValues.Enabled ||
+                        xssProtection == Constants.Headers.XxssProtectionValues.Disabled)
+                    {
+                        context.Response.Headers.Append("X-XSS-Protection", xssProtection);
+                    }
+                    
+                    if (configSection[Constants.Headers.Hsts] == "true")
+                    {
+                        app.UseHsts();
+                    } 
+                    
+                    await next();
+                });
+            }
+        }
+        return app;
+    }
+}
